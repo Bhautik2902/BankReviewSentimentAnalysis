54bde3d0a68f3e2b09db941dc8478e5e5308f18b
Bhautik2902 <bhautiksavaliya2000@gmail.com>
2024-10-22 13:56:13 -0400

    Implemented changes in the UI

--- bank_sense/BankSense/models.py
+++ bank_sense/BankSense/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    # Non-nullable fields
+    index = models.IntegerField()
+    source = models.CharField(max_length=50)
+    bank = models.CharField(max_length=100)
+    title = models.TextField()
+    review_text = models.TextField()
+
+    rating = models.FloatField(null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    url = models.URLField(max_length=500, null=True, blank=True)
+    source_type = models.CharField(max_length=50, null=True, blank=True)
+    review_sentiment = models.CharField(max_length=50)
+    sentiment_score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.bank} - {self.source} - {self.review_sentiment}"
+
--- bank_sense/BankSense/views.py
+++ bank_sense/BankSense/views.py
@@ -1,3 +1,55 @@
+import os
+import pandas as pd
+from django.conf import settings
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Review
+from datetime import datetime
 
-# Create your views here.
+
+# View to list all reviews
+def dashboard_view(request):
+    return render(request, 'BankSense/index.html')
+
+
+def store_data_in_db(request):
+    response = HttpResponse()
+    response.write("Already stored")
+    return response
+#     file_path = os.path.join(settings.BASE_DIR, 'BankSense', 'data', 'reddit_google_merged_data.xlsx')
+#     response.write(file_path)
+#
+#     try:
+#         df = pd.read_excel(file_path)
+#     except Exception as e:
+#         response.write(str(e))
+#
+#     for index, row in df.iterrows():
+#         # Convert the date field to a Python datetime object if necessary
+#         date_str = row['date']
+#         if pd.isnull(date_str):
+#             date_obj = None
+#         else:
+#             try:
+#                 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M')  # Adjust date format if needed
+#             except ValueError:
+#                 date_obj = None  # Handle any unexpected date formats
+#
+#         # Create and save the review object
+#         review = Review(
+#             index=row['Index'],
+#             source=row['source'],
+#             bank=row['bank'],
+#             title=row['title'],
+#             review_text=str(row['review_text']),  # Convert to string
+#             rating=row['rating'] if not pd.isnull(row['rating']) else None,
+#             date=date_obj,
+#             url=row['url'] if not pd.isnull(row['url']) else None,
+#             source_type=row['source_type'] if not pd.isnull(row['source_type']) else None,
+#             review_sentiment=row['review_sentiment'],
+#             sentiment_score=row['sentiment_score'],
+#         )
+#         review.save()
+#
+#     response.write("review stored successfully")
+#     return response
