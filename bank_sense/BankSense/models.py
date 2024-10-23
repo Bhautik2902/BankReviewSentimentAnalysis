278bc614e938dcb902f56ddca5a72fdc4774b5bf
Bhautik2902 <bhautiksavaliya2000@gmail.com>
2024-10-23 14:43:41 -0400

    Added the models for to support visulization in front-end

--- bank_sense/BankSense/models.py
+++ bank_sense/BankSense/models.py
@@ -19,3 +19,59 @@ class Review(models.Model):
     def __str__(self):
         return f"{self.bank} - {self.source} - {self.review_sentiment}"
 
+
+class VisuliData(models.Model):
+    bank_name = models.CharField(max_length=20)
+    total_reviews = models.IntegerField()
+    avg_rating = models.FloatField()
+    searched_st_service = models.CharField(max_length=50)
+    searched_query = models.TextField()
+
+    common_services = models.JSONField(null=True, blank=True)
+    positive_reviews = models.TextField(null=True, blank=True)
+    negative_reviews = models.TextField(null=True, blank=True)
+
+    def add_instance(self, instance):
+        if not self.common_services:
+            self.instances_list = []
+        self.instances_list.append(instance.to_dict())
+        self.save()
+
+    def get_instances(self):
+        return [ServiceModel.from_dict(item) for item in self.instances_list]
+
+    def get_review_list(self, listtype: int):
+        if self.positive_reviews and listtype == 1:
+            return self.positive_reviews.split(',')
+        elif self.negative_reviews and listtype == 0:
+            return self.negative_reviews.split(',')
+        return []
+
+    def set_positive_services_list(self, positive_reviews):
+        self.services = ','.join(positive_reviews)
+
+    def set_negative_services_list(self, negative_reviews):
+        self.services = ','.join(negative_reviews)
+
+
+class ServiceModel:
+    def __init__(self, name: str, positive: int, negative: int, neutral: int):
+        self.name = name
+        self.positive = positive
+        self.negative = negative
+        self.neutral = neutral
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'positive': self.positive,
+            'negative': self.negative,
+            'neutral': self.neutral
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data['name'], data['positive'], data['negative'], data['neutral'])
+
+    def __str__(self):
+        return f"Name: {self.name}"
--- bank_sense/BankSense/views.py
+++ bank_sense/BankSense/views.py
@@ -3,13 +3,56 @@ import pandas as pd
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Review
+from .models import Review, VisuliData
 from datetime import datetime
 
 
 # View to list all reviews
 def dashboard_view(request):
+    file_path = os.path.join(settings.BASE_DIR, 'BankSense', 'data', 'reddit_google_merged_data.xlsx')
+
+    service_name = 'Credit' #request.GET.get('service', None)
+    bank_name = 'RBC' # request.GET.get('bank', None)
+    search_query = '', # request.GET.get('query', None)
+
+    try:
+        df = pd.read_excel(file_path)
+        analyze_service_sentiment(df, bank_name, service_name, search_query)
+
+    except Exception as e:
+        print(str(e))
+
     return render(request, 'BankSense/index.html')
+
+
+def analyze_service_sentiment(df, bank_name, service_name, search_query=None):
+    # Initialize variables
+    sentiment_counter = 0
+    positive_reviews = []
+    negative_reviews = []
+    keywords_to_avoid = ['app', 'interface', 'ui', 'layout', 'design', 'update', 'fingertips', 'bug', 'fingerprint', 'version']
+
+    # Filter reviews by the given bank name and check for the service name in the review
+    bank_reviews = df[df['AppName'].str.split('_').str[0] == bank_name]
+
+    for _, row in bank_reviews.iterrows():
+        review = row['Review']
+        review = str(review).lower()
+
+        if service_name.lower() in review:
+            sentiment = row['Review sentiment']
+
+            # Increment or decrement sentiment_counter based on the sentiment
+            if sentiment == 'positive' and all(keyword not in review for keyword in keywords_to_avoid):
+                sentiment_counter += 1
+                if len(positive_reviews) < 5:  # Record max 5 positive reviews
+                    positive_reviews.append(review)
+            elif sentiment == 'negative' and all(keyword not in review for keyword in keywords_to_avoid):
+                sentiment_counter -= 1
+                if len(negative_reviews) < 5:
+                    negative_reviews.append(review)
+
+    return sentiment_counter, positive_reviews, negative_reviews
 
 
 def store_data_in_db(request):
